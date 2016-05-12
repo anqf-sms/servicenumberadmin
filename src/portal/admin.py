932771843f2b67ad3531d0954d9921edd27693d2
William An <wan@1080agile.com>
2016-05-12 12:38:10 +1000

    did history past time is not allowed

--- src/portal/admin.py
+++ src/portal/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django import forms
+
+from django.core.exceptions import ValidationError
 
 from portal.models import *
+from django.utils import timezone
 
 
 class ServiceSettingInlineAdmin(admin.TabularInline):
@@ -8,10 +12,95 @@ class ServiceSettingInlineAdmin(admin.TabularInline):
     max_num = 2
     extra = 2
 
+
+class DidAdminInline(admin.TabularInline):
+    verbose_name = 'History DID'
+    verbose_name_plural = 'History DID'
+    model = DidRecord
+    readonly_fields = ('did_number', 'begin_date_time', 'end_date_time', 'created_by', 'created', )
+    extra = 0
+    can_delete = False
+    empty_value_display = '- not set -'
+    def has_add_permission(self, request):
+        return False
+    def get_queryset(self, request):
+        qs = super(DidAdminInline, self).get_queryset(request)
+        return qs.filter(begin_date_time__lte=timezone.now()).order_by('-begin_date_time')
+
+# class DidAdminInlineFormSet(forms.BaseInlineFormSet):
+#     def clean(self):
+#         print '#'*10, 'clean called'
+#         super(DidAdminInlineFormSet, self).clean()
+#         for form in self.forms:
+#             if not hasattr(form, 'cleaned_data'):
+#                 continue
+#             print '@'*10, form
+#             data = self.cleaned_data
+#             for r in data:
+#                 print '>>>', r
+#                 if r != {}:
+#                     if r['begin_date_time']<timezone.now():
+#                         raise forms.ValidationError([{'begin_date_time': 'Past time is not allowed.'}])
+
+
+#             #Always return the cleaned data, whether you have changed it or not.
+#             #return data
+
+class DidAddForm(forms.ModelForm):
+    timeline = forms.DateTimeField(initial=timezone.now, label='timeline', widget = forms.HiddenInput())
+    class Meta:
+        model = DidRecord
+        fields = ('did_number', 'begin_date_time')
+
+    def has_changed(self, *args, **kwargs):
+        # print '@'*100
+        org_changed = super(DidAddForm, self).has_changed(*args, **kwargs)
+        if len(self.changed_data)==1 and self.changed_data[0]=='timeline':
+            changed = False
+        else:
+            changed = org_changed
+        # print '@'*100, org_changed, changed, self.changed_data
+        return changed
+
+    def clean(self):
+        print '$'*100
+        # print 'pk=', repr(self.instance.pk), 'did_number=', repr(self.instance.did_number), 'begin_date_time=', repr(self.instance.begin_date_time)
+        cleaned_data = super(DidAddForm, self).clean()
+        begin_date_time = cleaned_data.get('begin_date_time')
+        timeline = cleaned_data.get('timeline')
+        if begin_date_time is not None and begin_date_time<timeline:
+            raise forms.ValidationError([{'begin_date_time':'Past time not allowed.'}])
+        # del cleaned_data['timeline']
+        return cleaned_data
+
+
+class AddDidAdminInline(admin.TabularInline):
+    verbose_name = 'New DID'
+    verbose_name_plural = 'New DID'
+    model = DidRecord
+    form = DidAddForm
+    extra = 1
+    max_num = 1
+    def get_queryset(self, request):
+        qs = super(AddDidAdminInline, self).get_queryset(request)
+        return qs.filter(begin_date_time__gte=timezone.now())
+
 class ServiceNumberAdmin(admin.ModelAdmin):
     model = ServiceNumber
-    list_display = ('securidial', 'caller_number', 'remote_r', 'remote_l', )
-    inlines = (ServiceSettingInlineAdmin, )
+    list_display = ('securidial', 'did_number', 'remote_r', 'remote_l', )
+    inlines = (ServiceSettingInlineAdmin, AddDidAdminInline, DidAdminInline, )
+    save_on_top = True
+
+    def __init__(self, *args, **kwargs):
+        super(ServiceNumberAdmin, self).__init__(*args, **kwargs)
+
+    def save_related(self, request, form, formsets, change):
+        form.save_m2m()
+        for formset in formsets:
+            for f in formset:
+                if hasattr(f.instance, 'created_by_id') and f.instance.created_by_id is None:
+                    f.instance.created_by_id = request.user.pk
+            self.save_formset(request, form, formset, change=change)
 
 class ServiceSettingAdmin(admin.ModelAdmin):
     class Meta:
--- src/portal/models.py
+++ src/portal/models.py
@@ -6,19 +6,35 @@ from django.core.validators import RegexValidator
 from django.db.models.signals import *
 from django.dispatch.dispatcher import receiver
 
+from django.contrib.auth.models import User
+
+from django.core.exceptions import ValidationError
+
 # pybeanstalk imports
 import beanstalkc
 import simplejson
+from django.utils import timezone
 
-# Create your models here.
 
 class ServiceNumber(models.Model):
     securidial  = models.CharField(max_length=32, verbose_name="Service Number", validators=[RegexValidator(r'^13\d{8}$','Enter a valid service number','invalid')])
-    caller_number = models.CharField(max_length=32, verbose_name="Backend Number", validators=[RegexValidator(r'^04\d{8}$','Enter a valid backend number','invalid')])
     remote_r = models.CharField(max_length=32, verbose_name="Remote R", blank=True)
     remote_l = models.CharField(max_length=32, verbose_name="Remote L", blank=True)
+    def did_number(self):
+        did_records = self.did_records.filter(begin_date_time__lte=timezone.now()).order_by('-pk')
+        return did_records.first().did_number
     def __unicode__(self):
-          return '{} -> {}'.format(self.securidial, self.caller_number)
+          return '{} -> {}'.format(self.securidial, self.did_number())
+
+class DidRecord(models.Model):
+    service = models.ForeignKey(ServiceNumber, related_name="did_records", on_delete=models.CASCADE,)
+    did_number = models.CharField(max_length=32, verbose_name="DID Number", validators=[RegexValidator(r'^04\d{8}$','Enter a valid backend number','invalid')])
+    begin_date_time = models.DateTimeField()
+    end_date_time = models.DateTimeField(null=True, blank=True)
+    created_by = models.ForeignKey(User, related_name="did_records", )
+    created = models.DateTimeField(auto_now_add=True)
+    def __unicode__(self):
+          return '{}'.format(self.did_number)
 
 class ServiceSetting(models.Model):
     service = models.ForeignKey(ServiceNumber, related_name="settings", on_delete=models.CASCADE,)
@@ -32,16 +48,22 @@ class ServiceSetting(models.Model):
 
 
 def publish_data(service):
-    try:
+    if True:
+        did_records = service.did_records.all().order_by('-begin_date_time')
         data = {
             'table': 'cmssetting',
             'id': service.securidial,
-            'callerid': {
-                'securidial': service.securidial,
-                'caller_number': service.caller_number,
-                "remote_r": service.remote_r,
-                "remote_l": service.remote_l,
-            },
+            "remote_r": service.remote_r,
+            "remote_l": service.remote_l,
+            'did_records':[
+                {
+                    'did_number': r.did_number,
+                    'begin_date_time': str(r.begin_date_time), #timezone.localtime()
+                    'end_date_time': None if r.end_date_time is None else str(r.end_date_time),
+                    'created_by': r.created_by.username,
+                    'created': str(r.created.replace(microsecond=0)),
+                } for r in did_records
+            ],
             'callerid_as_settings': [
                 {
                     'type': setting.port_type,
@@ -53,18 +75,50 @@ def publish_data(service):
                 } for setting in service.settings.all()
             ],
         }
-        # print '#'*20, simplejson.dumps(data)
+        print '#'*20, data
+        print '#'*20, simplejson.dumps(data)
         beanstalk = beanstalkc.Connection(host='127.0.0.1', port=11300)
         beanstalk.use('cms.setting.update')
         beanstalk.using(), beanstalk.tubes()
         beanstalk.put(simplejson.dumps(data))
-    except:
-        pass
+    # except:
+    #     pass
+
+
+from threading import Thread
+import time
+def postpone(function):
+    def decorator(*args, **kwargs):
+        t = Thread(target = function, args=args, kwargs=kwargs)
+        t.daemon = False
+        t.start()
+    return decorator
+
+@postpone
+def refine_end_date_time(service):
+    time.sleep(0.5)
+
+    did_records = service.did_records.all().order_by('-begin_date_time')
+
+    for idx in range(len(did_records)):
+        did_record = did_records[idx]
+        # print '@'*10, did_record.begin_date_time, did_record.end_date_time
+        if idx == 0:
+            did_record.end_date_time = None
+        else:
+            # print 'bang!'
+            did_record.end_date_time = did_records[idx-1].begin_date_time
+        # print '@'*10, did_record.begin_date_time, did_record.end_date_time
+        did_record.save()
+
+    publish_data(service)
+
 
 @receiver(post_save, sender=ServiceNumber)
 def ServiceNumber_post_save_works(sender, instance, **kwargs):
     print '========ServiceNumber saved'
-    publish_data(instance)
+    refine_end_date_time(instance)
+
 
 @receiver(pre_save, sender=ServiceSetting)
 def ServiceSetting_pre_save_works(sender, instance, **kwargs):
@@ -77,23 +131,5 @@ def ServiceSetting_post_save_works(sender, instance, **kwargs):
     print '======== ServiceSetting saved'
     publish_data(instance.service)
 
-# {
-#     "table": "cmssetting",
-#     "id": 13451234,
-#     "callerid": {
-#         "securidial": 13451234,
-#         "caller_number": "0312345678",
-#         "remote_r": 0,
-#         "remote_l": 1
-#     },
-#     "callerid_as_settings": [
-#         {
-#             "type": "tcp",
-#             "port": 9307,
-#             "remote_as_ip": "10.100.0.2",
-#             "remote_receiver_ip": "192.168.10.100",
-#             "cms_description": "test"
-#         }
-#     ]
-# }
+
 
