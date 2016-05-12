76b7497c3b03f31f246131a5e9ab5b6210a757c2
William An <wan@1080agile.com>
2016-05-12 12:39:03 +1000

    postgresql database setting

--- src/servicenumberadmin/settings.py
+++ src/servicenumberadmin/settings.py
@@ -86,6 +86,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'servicenumberadmin',                      # Or path to database file if using sqlite3.
+#         # The following settings are not used with sqlite3:
+#         'USER': 'william',
+#         'PASSWORD': '1234qwer',
+#         'HOST': '',         # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#         'PORT': '',                      # Set to empty string for default.
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
